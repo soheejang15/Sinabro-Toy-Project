24d849c6fb5f710f0da57ea873325d29c0cd1fda
sohi_5 <38098147+soheejang15@users.noreply.github.com>
2019-03-21 23:10:50 +0900

    프로젝트 완성
    검색 기능 구현

--- mail.js
+++ mail.js
@@ -73,49 +73,59 @@ function domControl(containerName, tag, id, className, parrentNode, text, backgr
   parrentNode.appendChild(containerName);
 }
 
+let listIndexFlag = {
+  'home': true,
+  'customers': false,
+  'indox': false,
+  'settings': false,
+};
 
 // 사이드 바 클릭 이벤트 리스너 등록
-let clickSidebarIndex = document.querySelector(".sidebar-list");
 let sidebarFlag = false;
 let currentContent = 'index-inbox';
 let theContent = document.getElementById('content');
-clickSidebarIndex.addEventListener('click', function (e) {
-  if (sidebarFlag) document.querySelector(".selected-list-index").className = "list-index";
-  document.getElementById(e.target.id).className = "selected-list-index";
-  sidebarFlag = true;
+const listIndex = document.querySelectorAll(".list-index");
 
-  currentContent = e.target.id;
-  let theContentContainer = document.getElementById('content-container');
-  //let theIndexTitle = document.getElementById('index-title');
+for(let i = 0; i<listIndex.length; i++){
+  listIndex[i].addEventListener('click', function(e){
+    e.stopPropagation();
+    if (sidebarFlag) document.querySelector(".selected-list-index").className = "list-index";
+    document.getElementById(e.currentTarget.id).className = "selected-list-index";
+  
+    sidebarFlag = true;
 
-  theContentContainer.removeChild(document.getElementById('content'));
-  theContentContainer.removeChild(document.getElementById('index-title'));
-  switch (currentContent) {
-    case 'index-home':
-      let homeTitle;
-      domControl(homeTitle, "p", "index-title", false, theContentContainer, "Home", false);
-      let home;
-      domControl(home, "div", "content", false, theContentContainer, "home", false);
-      break;
-    case 'index-customers':
-      let customersTitle;
-      domControl(customersTitle, "p", "index-title", false, theContentContainer, "Customers", false);
-      let customers;
-      domControl(customers, "div", "content", false, theContentContainer, "customers", false);
-      break;
-    case 'index-inbox':
-      let inboxTitle;
-      domControl(inboxTitle, "p", "index-title", false, theContentContainer, "Inbox", false);
-      theContentContainer.appendChild(theContent);
-      break;
-    case 'index-settings':
-    let settingsTitle;
-      domControl(settingsTitle, "p", "index-title", false, theContentContainer, "Settings", false);
-      let settings;
-      domControl(settings, "div", "content", false, theContentContainer, "settings", false);
-      break;
+    currentContent = e.currentTarget.id;
+    let theContentContainer = document.getElementById('content-container');
+
+    theContentContainer.removeChild(document.getElementById('content'));
+    theContentContainer.removeChild(document.getElementById('index-title'));
+    switch (currentContent) {
+      case 'index-home':
+        let homeTitle;
+        domControl(homeTitle, "p", "index-title", false, theContentContainer, "Home", false);
+        let home;
+        domControl(home, "div", "content", false, theContentContainer, "home", false);
+        break;
+      case 'index-customers':
+        let customersTitle;
+        domControl(customersTitle, "p", "index-title", false, theContentContainer, "Customers", false);
+        let customers;
+        domControl(customers, "div", "content", false, theContentContainer, "customers", false);
+        break;
+      case 'index-inbox':
+        let inboxTitle;
+        domControl(inboxTitle, "p", "index-title", false, theContentContainer, "Inbox", false);
+        theContentContainer.appendChild(theContent);
+        break;
+      case 'index-settings':
+        let settingsTitle;
+        domControl(settingsTitle, "p", "index-title", false, theContentContainer, "Settings", false);
+        let settings;
+        domControl(settings, "div", "content", false, theContentContainer, "settings", false);
+        break;
   }
-});
+  });
+}
 
 // 태그 수 카운트
 let numTag = {};
@@ -179,60 +189,122 @@ for (let i in mailData) {
 }
 
 // 메일 리스트 클릭 이벤트 리스너
-let selectMessageContent = document.querySelector('#message-list');
 let messageFlag = false;
-selectMessageContent.addEventListener('click', function (e) {
+let mailIndex = document.querySelectorAll(".message-content");
+mailIndex.forEach(i => {
+  i.addEventListener('click',function(e){
+    e.stopImmediatePropagation();
+    let currentMailData = mailData[e.currentTarget.id];
 
-  let currentMailData = mailData[e.target.id];
+    if (messageFlag) document.querySelector(".selected-message-content").className = "message-content";
+    document.getElementById(e.currentTarget.id).className = "selected-message-content";
+    messageFlag = true;
 
-  if (messageFlag) document.querySelector(".selected-message-content").className = "message-content";
-  document.getElementById(e.target.id).className = "selected-message-content";
-  messageFlag = true;
+    let theUserBox = document.getElementById("user");
+    let theUpperBox = document.querySelector("#user .upper-box");
+    let theLowerBox = document.querySelector("#user .lower-box");
 
-  let theUserBox = document.getElementById("user");
-  let theUpperBox = document.querySelector("#user .upper-box");
-  let theLowerBox = document.querySelector("#user .lower-box");
+    theUserBox.removeChild(theUpperBox);
+    theUserBox.removeChild(theLowerBox);
 
-  theUserBox.removeChild(theUpperBox);
-  theUserBox.removeChild(theLowerBox);
+    let upperBox = document.createElement("div");
+    upperBox.setAttribute("class", "upper-box");
 
-  let upperBox = document.createElement("div");
-  upperBox.setAttribute("class", "upper-box");
+    // 선택된 메일 태그 표시
+    if (currentMailData.tag !== '') {
+      let theTag;
+      domControl(theTag, "div", currentMailData.tag, false, upperBox, currentMailData.tag, false);
+    }
 
-  // 선택된 메일 태그 표시
-  if (currentMailData.tag !== '') {
-    let theTag;
-    domControl(theTag, "div", currentMailData.tag, false, upperBox, currentMailData.tag, false);
+    // 선택된 메일 타이틀 표시
+    let theTitle;
+    domControl(theTitle, "span", "title", false, upperBox, currentMailData.title, false);
+
+    theUserBox.appendChild(upperBox);
+
+    let lowerBox = document.createElement("div");
+    lowerBox.setAttribute("class", "lower-box");
+
+    let theImg;
+    domControl(theImg, "div", "img", false, lowerBox, false, currentMailData.img);
+
+
+    let theName;
+    domControl(theName, "span", "name", false, lowerBox, currentMailData.from, false);
+
+    let theDate;
+    domControl(theDate, "span", "date", false, lowerBox, currentMailData.date, false);
+
+    let to;
+    domControl(to, "div", "to", false, lowerBox, "to me", false);
+
+    theUserBox.appendChild(lowerBox);
+
+    let theTextBox = document.getElementById("text");
+    let theTextContent = document.querySelector("#text #mail-content");
+    theTextBox.removeChild(theTextContent);
+
+    let theText;
+    domControl(theText, "p", "mail-content", false, theTextBox, currentMailData.text,false);
+  })
+});
+
+
+// 메일 리스트 검색
+
+const theInput = document.getElementById("message-search");
+let theTerm;
+theInput.addEventListener('keyup', function(e){
+  theTerm = e.target.value.trim();
+  console.log(theTerm);
+  document.getElementById("message-list").innerHTML = "";
+  let theSearchedList = mailData.filter(i => i.title.indexOf(theTerm) >= 0);
+
+  for (let i in theSearchedList) {
+    let currentSmallData = theSearchedList[i];
+  
+    let mailContent = document.createElement("li");
+    mailContent.setAttribute("class", "message-content");
+    mailContent.setAttribute("id", i);
+  
+    let mailTag = document.createElement("div");
+    mailTag.setAttribute("class", "mail-tag-list");
+    mailTag.setAttribute("id", i + "Tag");
+  
+    document.getElementById("message-list").appendChild(mailContent);
+    mailContent.appendChild(mailTag);
+  
+    let tagColor = document.getElementById(i + "Tag");
+    switch (mailData[i].tag) {
+      case "sales":
+        tagColor.className += '-sales';
+        break;
+      case "marketing":
+        tagColor.className += '-marketing';
+        break;
+      case "design":
+        tagColor.className += '-design';
+        break;
+      case "frontend":
+        tagColor.className += '-frontend';
+        break;
+    }
+  
+    let container = document.createElement("div");
+    mailContent.appendChild(container);
+    container.style.display = 'inline-block';
+    container.style.position = 'relative';
+  
+    let mailName;
+    domControl(mailName, "span",false, "name", container, currentSmallData.from, false);
+  
+    let mailDate;
+    domControl(mailDate, "span", false, "date", mailContent, currentSmallData.date, false);
+  
+    let mailTitle;
+    domControl(mailTitle, "div", false, "title", container, currentSmallData.title, false);
+  
+  
+  
   }
-
-  // 선택된 메일 타이틀 표시
-  let theTitle;
-  domControl(theTitle, "span", "title", false, upperBox, currentMailData.title, false);
-
-  theUserBox.appendChild(upperBox);
-
-  let lowerBox = document.createElement("div");
-  lowerBox.setAttribute("class", "lower-box");
-
-  let theImg;
-  domControl(theImg, "div", "img", false, lowerBox, false, currentMailData.img);
-
-
-  let theName;
-  domControl(theName, "span", "name", false, lowerBox, currentMailData.from, false);
-
-  let theDate;
-  domControl(theDate, "span", "date", false, lowerBox, currentMailData.date, false);
-
-  let to;
-  domControl(to, "div", "to", false, lowerBox, "to me", false);
-
-  theUserBox.appendChild(lowerBox);
-
-  let theTextBox = document.getElementById("text");
-  let theTextContent = document.querySelector("#text #content");
-  theTextBox.removeChild(theTextContent);
-
-  let theText;
-  domControl(theText, "p", "content", false, theTextBox, currentMailData.text,false);
 });
